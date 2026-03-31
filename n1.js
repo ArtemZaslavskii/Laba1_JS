38d1c89c392255e32da5180b5d7b59878258cfe9
TemaZaslavskii <tema.zaslavskii@gmail.com>
2026-03-31 23:31:25 +0700

    :>

---
+++ n1.js
@@ -0,0 +1,15 @@
+let numberVar = 42;
+let stringVar = "Hello";
+let booleanVar = true;
+let arrayVar = [1, 2, 3];
+let objectVar = {name: "John"};
+let nullVar = null;
+let undefinedVar;
+
+console.log("numberVar:", typeof(numberVar));
+console.log("stringVar:", typeof(stringVar));
+console.log("booleanVar:", typeof(booleanVar));
+console.log("arrayVar:", typeof(arrayVar));
+console.log("objectVar:", typeof(objectVar));
+console.log("nullVar:", typeof(nullVar));
+console.log("undefinedVar:", typeof(undefinedVar));
