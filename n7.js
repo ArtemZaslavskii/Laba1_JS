bdebae2ac46f8cf438c29f3b1f0baace1b0f90b8
TemaZaslavskii <tema.zaslavskii@gmail.com>
2026-04-01 13:23:51 +0700

    есть такое дело

---
+++ n7.js
@@ -0,0 +1,8 @@
+let a = "12";
+let b = "7.15";
+
+let A = Number(a);
+let B = Math.round(Number(b));
+let result = A % B;
+
+console.log(result);
---
+++ n9.js
@@ -0,0 +1,4 @@
+let email = "ybiica.smerti@gmail.ru";
+
+if (email.includes("@")) {
+    console.log("Есть такое дело(@)");}
